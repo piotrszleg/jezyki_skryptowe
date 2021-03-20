66545318c3e0b1345013cd3b582f2143048b7b8c
piotrszleg <szlegp@gmail.com>
2021-03-20 21:15:56 +0100

    Miniaturki, pasek na pełną szerkokść ekranu.

---
+++ src/renderer/Thumbnail.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+});
+
+export default function Thumbnail() {
+    const classes = useStyles();
+
+    return (
+        <Card className={classes.root}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    alt="Contemplative Reptile"
+                    height="140"
+                    image="https://upload.wikimedia.org/wikipedia/commons/2/27/MnistExamples.png"
+                    title="Contemplative Reptile"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        MNIST
+                    </Typography>
+                    <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="p"
+                    >
+                        Labeled hand written numbers.
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                    Download
+                </Button>
+                <Button size="small" color="primary">
+                    Open
+                </Button>
+            </CardActions>
+        </Card>
+    );
+}

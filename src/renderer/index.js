7d228806f704ffd2a942cee3701256adcac2e2c5
piotrszleg <szlegp@gmail.com>
2021-03-20 21:15:24 +0100

    Usunięcie kodu z innej wersji material design.

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Page from './Page.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const list = MDCList.attachTo(document.querySelector('.mdc-list'));
-  list.wrapFocus = true;
-});
-
-
 ReactDOM.render(
   <React.StrictMode>
     <Page></Page>
